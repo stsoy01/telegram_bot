4a055bd219f84d9429aaaf9e468a6860897f1903
stsoy01 <tsoy@perco.ru>
2024-12-25 16:00:17 +0300

    upd

---
+++ frontend/src/app/components/carousel/carousel.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from "@angular/core";
+import {NgOptimizedImage} from "@angular/common";
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.scss'],
+  imports: [
+    NgOptimizedImage
+  ],
+  standalone: true
+})
+
+export class CarouselComponent  {
+
+}
--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { TelegramService } from '../../services/telegram.service';
 import {HeaderComponent} from "../../components/header/header.component";
 import {InfiniteClientsComponent} from "../../components/infinite-clients/infinite-clients.component";
+import {CarouselComponent} from "../../components/carousel/carousel.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     HeaderComponent,
-    InfiniteClientsComponent
+    InfiniteClientsComponent,
+    CarouselComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
